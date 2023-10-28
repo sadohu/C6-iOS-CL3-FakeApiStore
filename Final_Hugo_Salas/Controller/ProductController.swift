7285c13645c2cf64ba5b4581d93415492a7eae61
sadohu <sadohu26@yahoo.com>
2023-10-28 01:20:22 -0500

    [Add] Show data in View [Fix] Main thread bug, detailView design

--- Final_Hugo_Salas/Controller/ProductController.swift
+++ Final_Hugo_Salas/Controller/ProductController.swift
@@ -29,26 +29,32 @@ class ProductController {
     }
     
     static func getProductById(idProduct id : Int, delegate : DataLoadedDelegate){
-        var item : Product? = nil;
-        let url = URL(string: self.URL_API + String(id));
-        let task = URLSession.shared.dataTask(with: url!) { data, urlResponse, error in
-            do {
-               if let data = data {
-                   let decodeData = try JSONDecoder().decode(Product.self, from: data);
-                   let item = decodeData;
-               }
-               DispatchQueue.main.async {
-                   if let item = item{
-                       delegate.itemLoadedSuccessfully(data: item);
-                   }else{
+        var item: Product? = nil
+           let url = URL(string: self.URL_API + String(id))
+           let task = URLSession.shared.dataTask(with: url!) { data, _, error in
+               if let error = error {
+                   print("Error al cargar datos: \(error)")
+                   DispatchQueue.main.async {
                        delegate.manageNilObject();
                    }
+               } else if let data = data {
+                   do {
+                       let decodeData = try JSONDecoder().decode(Product.self, from: data)
+                       item = decodeData
+                       DispatchQueue.main.async {
+                           if let item = item {
+                               delegate.itemLoadedSuccessfully(data: item);
+                           }
+                       }
+                   } catch {
+                       print("Error al decodificar datos: \(error)")
+                       DispatchQueue.main.async {
+                           delegate.manageNilObject();
+                       }
+                   }
                }
-           } catch {
-               print(error.localizedDescription);
            }
-        }
-        task.resume();
+           task.resume()
     }
     
     static func loadImage(fromURL imgURL : String, into imageView: UIImageView){
@@ -66,6 +72,12 @@ class ProductController {
         } else {
             print("Error URL inválido")
         }
+    }
+    
+    static func showAlert(message : String, viewController : UIViewController){
+        let alert = UIAlertController(title: "SISTEMA", message: message, preferredStyle: .alert);
+        alert.addAction(UIAlertAction(title: "Aceptar", style: .default));
+        viewController.present(alert, animated: true);
     }
     
 }
--- Final_Hugo_Salas/View/DetailViewController.swift
+++ Final_Hugo_Salas/View/DetailViewController.swift
@@ -12,30 +12,61 @@ class DetailViewController: UIViewController, DataLoadedDelegate {
     @IBOutlet weak var idProduct: UILabel!
     @IBOutlet weak var titleProduct: UILabel!
     @IBOutlet weak var priceProduct: UILabel!
-    @IBOutlet weak var descriptionProduct: UILabel!
+    @IBOutlet weak var descriptionProduct: UITextView!
     @IBOutlet weak var categoryProduct: UILabel!
     @IBOutlet weak var rateProduct: UILabel!
     @IBOutlet weak var countProduct: UILabel!
     @IBOutlet weak var imageProduct: UIImageView!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad();
+        setNotInfo();
     }
     
     @IBAction func btnFind(_ sender: UIButton) {
-        
+        let id = Int(txtIdProduct.text ?? "0") ?? 0;
+        if(id == 0){
+            ProductController.showAlert(message: "Ingrese un código válido (Número entero y mayor a cero).", viewController: self);
+            setNotInfo();
+            return;
+        }
+        ProductController.getProductById(idProduct: id, delegate: self);
     }
     
     func itemLoadedSuccessfully(data: Product) {
-        <#code#>
+        idProduct.text = String(data.id);
+        titleProduct.text = data.title;
+        priceProduct.text = String(data.price);
+        descriptionProduct.text = data.description;
+        categoryProduct.text = data.category;
+        rateProduct.text = String(data.rating.rate);
+        countProduct.text = String(data.rating.count);
+        ProductController.loadImage(fromURL: data.image, into: imageProduct);
+        txtIdProduct.text = "";
     }
     
     func manageNilObject() {
-        <#code#>
+        ProductController.showAlert(message: "No se encontró resultados con el código ingresado.", viewController: self);
+        setNotInfo();
+    }
+    
+    @IBAction func btnBack(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true);
+    }
+    
+    func setNotInfo(){
+        txtIdProduct.text = "";
+        idProduct.text = "";
+        titleProduct.text = "";
+        priceProduct.text = "";
+        descriptionProduct.text = "";
+        categoryProduct.text = "";
+        rateProduct.text = "";
+        countProduct.text = "";
+        imageProduct.image = nil;
     }
     
     func listLoadedSuccessfully(data: [Product]) {
-        <#code#>
     }
 
 }
